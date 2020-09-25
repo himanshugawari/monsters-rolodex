9b4fd4e5bd2cadaf9cb64e762418865560cc9ad3
unknown <himanshu25031991@gmail.com>
2020-09-25 20:28:10 +0530

    Updates

---
+++ precache-manifest.22531bb204efb8a9893a067c330a0d0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6514e1c4004534ecfd53f75876cb73b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "65cc79bd2434f6a2ccc0",
+    "url": "/monsters-rolodex/static/css/main.a956473a.chunk.css"
+  },
+  {
+    "revision": "57595fc6caf45f548129",
+    "url": "/monsters-rolodex/static/js/2.c272e29f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.c272e29f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65cc79bd2434f6a2ccc0",
+    "url": "/monsters-rolodex/static/js/main.ac76728e.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
